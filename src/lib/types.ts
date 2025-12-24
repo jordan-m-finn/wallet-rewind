3763581146737c6820f5b80897e187e04be04f70
jordan-m-finn <jordan.m.finn17@gmail.com>
2025-12-24 18:47:25 -0500

    Define type for supported chains (EVM)

---
+++ src/lib/types.ts
@@ -0,0 +1,20 @@
+// RawTransaction - what Covalent returns (will slim down)
+// WalletRecap - your aggregated output
+// Nameplate - the badges/categories
+
+// Supported chains with their Covalent IDs:
+// key (chain_name): value (chain_id)
+const SUPPORTED_CHAINS = {
+    "eth-mainnet": 1,
+    "eth-sepolia": 11155111,
+    "base-mainnet": 8453,
+    "base-sepolia": 84532,
+    "polygon-mainnet": 137,
+    "polygon-amoy": 80002,
+    "bnb": 56,
+    "bnb-testnet": 97,
+    "zkSync-Era": 324,
+    "zkSync-Era-testnet": 4002,
+    "arbitrum-one": 42161,
+    "arbitrum-sepolia": 421614,
+}
