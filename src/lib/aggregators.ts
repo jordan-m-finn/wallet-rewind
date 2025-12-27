2572bce171a8aaa6095658e594eb13978cfec2da
jordan-m-finn <jordan.m.finn17@gmail.com>
2025-12-27 17:13:47 -0500

    Draft logic for 1st aggregator function

--- src/lib/aggregators.ts
+++ src/lib/aggregators.ts
@@ -1,1 +1,16 @@
 // Goal: Take raw txns -> compute most transacted token, tx count, unique contracts interacted with
+// Functions needed:
+// countTransactionsByChain()
+// findMostTransactedToken()
+// countUniqueContracts()
+// countNFTTransfers()
+// calculateTotalGasSpent()
+// assignNameplates()
+
+export function countTransactionsByChain(_transactions: Transaction[]): Record<string, number> {
+    const counts: Record<string, number> = {};
+    // loop
+    for (let i = 0; i < _transactions.chainName.size; i++) {
+        counts[key] = (counts[key] ?? 0) + 1;
+    }
+}
