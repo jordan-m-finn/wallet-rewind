e74956a710d5d43d4e811ee0fee9df1dd252bc35
jordan-m-finn <jordan.m.finn17@gmail.com>
2026-01-03 23:57:59 -0800

    Fix typo in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { WalletConnectButton } from './ConnectButton'
 
 export function Header() {
     return (
-        <header classname="flex justify-between items-center p-4">
+        <header className="flex justify-between items-center p-4">
             <span className="font-bold text-xl">WalletRewind</span>
             <WalletConnectButton />
         </header>
