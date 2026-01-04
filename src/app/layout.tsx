e925f2f122faf570c6bcf1ea5f00029eb7597bb0
jordan-m-finn <jordan.m.finn17@gmail.com>
2026-01-03 16:03:58 -0800

    Add header component to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+            <Header />
+            {children}
+        </Providers>
       </body>
     </html>
   );
